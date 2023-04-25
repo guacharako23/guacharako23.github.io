67b07f8cb9d7872dafbe368985a8930a55eae246
Ronald_Barraza <87039930+guacharako23@users.noreply.github.com>
2023-04-24 20:39:24 -0500

    Add files via upload
    Diseñe un sitio web totalmente responsive cuyo contenido principal será cargado desde una API.
    
    incluya header
    la información principal del sitio está constituida por 18 cards organizadas de la siguiente manera:
    Para la resolución mas grande de pantalla: 3 cards en cada fila. El sitio contará con 6 filas para un total de 18 cards. 
    Para la resolución mas pequeña de pantalla: se deben mostrar las 18 cards una a una en una sola columna
         3. Incluya un footer con información del desarrollador.
         4. Cada una de las cards debe incluir imagen y titulo cargado de la api:
     https://jsonplaceholder.typicode.com/photos
        5. Puede usar las siguientes API opcional, en ese caso la card debe incluir imagen y nombre del personaje:
    https://rickandmortyapi.com/api/character
        6. Debe entregar un archivo de texto con links del repositorio en linea y despliegue.
        7. Incluya una sección de filtro tipo lista desplegable (select) donde se carguen los titulos/nombres de las cards como opciones.  al seleccionar una de ellas se de mostrar solo esa card. una de las opciones debe permitir ver todas las 18 cards.

---
+++ index.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+	cards();
+	select();
+});
+function select(){
+	
+	fetch('https://rickandmortyapi.com/api/character')
+			.then(response => response.json())
+			.then(data => {
+				// console.log(data);
+				for (let i = 0; i < 18; i++) {
+					const personaje = data.results[i];
+					const select = document.getElementById('filter-select');									
+					select.innerHTML+= `<option value="${personaje.id}">${personaje.name}</option>`;					
+				}
+			
+			})
+			.catch(error => console.log(error));
+}
+function cards(){
+	fetch('https://rickandmortyapi.com/api/character')
+	.then(response => response.json())
+	.then(data => {
+		for (let i = 0; i < 18; i++) {
+			const personaje = data.results[i];
+			const container = document.getElementById('cards-container');
+			container.innerHTML += `
+				<div class="col-lg-4 col-md-6 mb-4">
+					<div class="card h-100">
+						<img src="${personaje.image}" class="card-img-top" alt="${personaje.name}">
+						<div class="card-body">
+							<h5 class="card-title">${personaje.name}</h5>
+							<p class="card-text">Especie: ${personaje.species}</p>
+							<p class="card-text">Genero: ${personaje.gender}</p>
+							<p class="card-text">¿Alive o Dead?: ${personaje.status}</p>
+						</div>
+					</div>
+				</div>
+			`;
+		}
+	})
+	.catch(error => console.log(error));
+}
+function cambio(id){
+	console.log(id);
+	fetch('https://rickandmortyapi.com/api/character')
+	.then(response => response.json())
+	.then(data => {
+		for (let i = 0; i < 18; i++) {
+			const personaje = data.results[i];
+			console.log("ID: "+id+"Personaje: "+personaje.id);
+			if(id!='' && personaje.includes(id)){
+				console.log("1..");
+				const container = document.getElementById('cards-container');
+				container.innerHTML += `
+					<div class="col-lg-4 col-md-6 mb-4">
+						<div class="card h-100">
+							<img src="${personaje.image}" class="card-img-top" alt="${personaje.name}">
+							<div class="card-body">
+								<h5 class="card-title">${personaje.name}</h5>
+								<p class="card-text">Especie: ${personaje.species}</p>
+								<p class="card-text">Genero: ${personaje.gender}</p>
+								<p class="card-text">¿Alive o Dead?: ${personaje.status}</p>
+							</div>
+						</div>
+					</div>
+				`;
+			}else{
+				console.log("2..");
+				const container = document.getElementById('cards-container');
+				container.innerHTML += `
+					<div class="col-lg-4 col-md-6 mb-4">
+						<div class="card h-100">
+							<img src="${personaje.image}" class="card-img-top" alt="${personaje.name}">
+							<div class="card-body">
+								<h5 class="card-title">${personaje.name}</h5>
+								<p class="card-text">Especie: ${personaje.species}</p>
+								<p class="card-text">Genero: ${personaje.gender}</p>
+								<p class="card-text">¿Alive o Dead?: ${personaje.status}</p>
+							</div>
+						</div>
+					</div>
+				`;
+			}
+			
+		}
+	})
+	.catch(error => console.log(error));	
+}
+
+
+
